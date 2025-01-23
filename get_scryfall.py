6e746e598aa4845a141da1ad80b827935155a92c
TRJackson998 <t.r.jackson998@gmail.com>
2025-01-22 16:24:05 -0800

    Documentation

--- get_scryfall.py
+++ get_scryfall.py
@@ -1,7 +1,26 @@
 """
 Get Scryfall Data
 =================
-Use Scryfall's api to pull a json file containing every card object on Scryfall in English
+Use Scryfall's api to pull a json file containing card objects on Scryfall
+
+File Types
+----------
+ORACLE
+    A JSON file containing one Scryfall card object for each Oracle ID on Scryfall.
+    The chosen sets for the cards are an attempt to return the most up-to-date 
+    recognizable version of the card.
+UNIQUE
+    A JSON file of Scryfall card objects that together contain all unique artworks.
+    The chosen cards promote the best image scans.
+DEFAULT
+    A JSON file containing every card object on Scryfall in English or the printed 
+    language if the card is only available in one language.
+ALL
+    A JSON file containing every card object on Scryfall in every language.
+RULINGS
+    A JSON file containing all Rulings on Scryfall. Each ruling refers to cards via an `oracle_id`.
+
+Reference https://scryfall.com/docs/api/bulk-data
 """
 
 from enum import Enum
@@ -10,14 +29,19 @@ import requests
 
 # Define an Enum for the type options
 class BulkDataType(Enum):
+    """
+    Restrict the types of files that can be requested
+    """
+
     ORACLE = "oracle_cards"
-    DEFAULT = "default_cards"
     UNIQUE = "unique_artwork"
+    DEFAULT = "default_cards"
     ALL = "all_cards"
     RULINGS = "rulings"
 
 
 def main(data_type: BulkDataType):
+    """Pull the bulk data json file from Scryfall"""
     url = "https://api.scryfall.com/bulk-data"
 
     # Fetch bulk data info
@@ -28,6 +52,7 @@ def main(data_type: BulkDataType):
             if bulk_data["type"] == data_type.value:
                 file_url = bulk_data["download_uri"]
                 file_response = requests.get(file_url, timeout=60 * 5)
+
                 if file_response.status_code == 200:
                     file_name = f"{data_type.value}.json"
                     with open(file_name, "wb") as file:
